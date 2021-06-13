be5369751e858100d2c5f6c73018dab546fa2dcf
Nahuel Lopez <lopeznahuel1999@gmail.com>
2021-06-13 00:11:13 -0300

    alert

--- src/components/Asientos.js
+++ src/components/Asientos.js
@@ -4,7 +4,7 @@ import Modal from 'react-bootstrap/Modal';
 import {useHistory} from "react-router-dom";
 import {AuthContext} from "../services/auth";
 
-const array20 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+const array20 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40]
 
 function Asientos(props) {
     const [select, setSelect] = useState();
@@ -42,7 +42,7 @@ function Asientos(props) {
 
         {
             array20.map((item,index)=> <Button onClick={ () => setSelect(index + 1) } 
-            className="amarillos"
+            className="col-2"
             disabled={validarAsiento(index+1)}
             variant={props.reservados && validarAsiento(index+1) ? "danger" : "warning"} >{index + 1 }</Button>)
         }
