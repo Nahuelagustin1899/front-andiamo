929e7a5976c28ab0d3f6ab7e84060891c2aeddfa
Nahuel Lopez <lopeznahuel1999@gmail.com>
2021-06-15 00:35:11 -0300

    jwt

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,5 +3,5 @@ export const API = "https://andiamo-back.herokuapp.com/api";
 export const FETCH_HEADERS = {
     'Content-Type': 'application/json',
     'X-Requested-With': 'XMLHttpRequest',
-    'Autorization': localStorage.getItem('user') != undefined ? 'bearer ' + localStorage.getItem('user') : ''
+    'Autorization': localStorage.getItem('user') !== undefined ? 'bearer ' + localStorage.getItem('user') : ''
 };
