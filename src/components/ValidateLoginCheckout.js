d709beccfc689ec43ae36dd470bd6c8f7e711614
Nahuel Lopez <lopeznahuel1999@gmail.com>
2021-06-19 17:16:57 -0300

    Editar check

--- src/components/ValidateLoginCheckout.js
+++ src/components/ValidateLoginCheckout.js
@@ -8,204 +8,204 @@ const ValidateLoginCheckout = () => {
 
   const history = useHistory();
 
- return  <Formik
-      initialValues={{ 
-          nombre: "", 
-          apellido: "",
-          email: "",
-          dni:"",
-          provincia:"",
-          expiracion:"",
-          numTarj:"",
-          codigo:""
-        }}
-        
-      onSubmit={(values, { setSubmitting }) => {
-      
-        history.push("/");
-        setTimeout(() => {
-          
-          console.log("Formulario enviado con éxito", values);
-          setSubmitting(false);
-          
-        }, 500);
-        
-      }}
-      
-    
-      validationSchema={Yup.object().shape({
-          nombre: Yup.string()
-          .required("El campo nombre no puede estar vacío"),
-          apellido: Yup.string()
-          .required("El campo apellido no puede estar vacío"),
-          email: Yup.string()
-          .email("El email no es válido")
-          .required("El campo email no puede estar vacío"),
-          dni: Yup.number()
-          .required("El campo DNI no puede estar vacío"),
-          numTarj: Yup.number()
-          .required("El campo número de tarjeta no puede estar vacío"),
-          expiracion: Yup.date()
-          .required("El campo fecha de expiración no puede estar vacío"),
-          codigo: Yup.number()
-          .required("El campo fecha de expiración no puede estar vacío")   
-      })}
-    >
-      
-      {props => {
-        const {
-          values,
-          touched,
-          errors,
-          handleChange,
-          handleBlur,
-          handleSubmit
-        } = props;
+  return <Formik
+    initialValues={{
+      nombre: "",
+      apellido: "",
+      email: "",
+      dni: "",
+      provincia: "",
+      expiracion: "",
+      numTarj: "",
+      codigo: ""
+    }}
 
-     
-        
-        return (
-            
-          <form onSubmit={handleSubmit}>
-            <div className="row global">
-                <div className="col-md-12 mb-3">
-                    <label htmlFor="nombre">Nombre</label>
-                    <input
-                        name="nombre"
-                        type="text"
-                        placeholder="Nahuel"
-                        value={values.nombre}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        className="form-control"
-                        required
-                      />
-                      {errors.nombre && touched.nombre && (
-                        <div className="alert alert-danger mt-1">{errors.nombre}</div>
-                     )}
-                </div>
-                <div className="col-md-12 mb-3">
-                    <label htmlFor="apellido">Apellido</label>
-                    <input
-                        name="apellido"
-                        type="text"
-                        placeholder="Lopez"
-                        value={values.apellido}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        className='form-control'
-                        required
-                      />
-                      {errors.apellido && touched.apellido && (
-                        <div className="alert alert-danger mt-1">{errors.apellido}</div>
-                     )}
-                </div>
-                <div className="col-md-12 mb-3">
-                    <label htmlFor="email">Email</label>
-                    <input
-                        name="email"
-                        type="email"
-                        placeholder="nahuel@davinci.edu.ar"
-                        value={values.email}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        className='form-control'
-                        required
-                      />
-                      {errors.email && touched.email && (
-                        <div className="alert alert-danger mt-1">{errors.email}</div>
-                     )}
-                </div>
-                <div className="col-md-12 mb-3">
-                    <label htmlFor="dni">DNI</label>
-                    <input
-                        name="dni"
-                        type="text"
-                        placeholder="11.222.333"
-                        value={values.dni}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        className='form-control'
-                        required
-                      />
-                      {errors.dni && touched.dni && (
-                        <div className="alert alert-danger mt-1">{errors.dni}</div>
-                     )}
-                </div>
-                <div className="col-md-12 mb-3">
-                    <label htmlFor="provincia">Provincia</label>
-                        <select className="custom-select d-block w-100" id="provincia" placeholder="Elegi una provincia">
-                            <option>Buenos Aires</option>
-                            <option>Chaco</option>
-                            <option>Formosa</option>
-                            <option>Mendoza</option>
-                            <option>San Luis</option>
-                            <option>Salta</option>
-                            <option>Entre Ríos</option>
-                        </select>
-                        {errors.provincia && touched.provincia && (
-                        <div className="alert alert-danger mt-1">{errors.provincia}</div>
-                     )}
-                </div>
-                <div className="col-md-12 mb-3">
-                    <label htmlFor="numTarj">Número de tarjeta</label>
-                    <input
-                        name="numTarj"
-                        type="text"
-                        placeholder="1111 2222 3333 4444"
-                        value={values.numTarj}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        className='form-control'
-                        required
-                      />
-                      {errors.numTarj && touched.numTarj && (
-                        <div className="alert alert-danger mt-1">{errors.numTarj}</div>
-                     )}
-                </div>
-                <div className="col-md-12 mb-3">
-                    <label htmlFor="expiracion">Fecha de expiración</label>
-                    <input
-                        name="expiracion"
-                        type="month"
-                        placeholder="Fecha"
-                        value={values.expiracion}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        className='form-control'
-                        required
-                      />
-                      {errors.expiracion && touched.expiracion && (
-                        <div className="alert alert-danger mt-1">{errors.expiracion}</div>
-                     )}
-                </div>
-                <div className="col-md-12 mb-3">
-                    <label htmlFor="codigo">Código de seguridad</label>
-                    <input
-                        name="codigo"
-                        type="text"
-                        placeholder="123"
-                        value={values.codigo}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        className='form-control'
-                        required
-                      />
-                      {errors.codigo && touched.codigo && (
-                        <div className="alert alert-danger mt-1">{errors.codigo}</div>
-                     )}
-                </div>
+    onSubmit={(values, { setSubmitting }) => {
+
+      history.push("/");
+      setTimeout(() => {
+
+        console.log("Formulario enviado con éxito", values);
+        setSubmitting(false);
+
+      }, 500);
+
+    }}
+
+
+    validationSchema={Yup.object().shape({
+      nombre: Yup.string()
+        .required("El campo nombre no puede estar vacío"),
+      apellido: Yup.string()
+        .required("El campo apellido no puede estar vacío"),
+      email: Yup.string()
+        .email("El email no es válido")
+        .required("El campo email no puede estar vacío"),
+      dni: Yup.number()
+        .required("El campo DNI no puede estar vacío"),
+      numTarj: Yup.number()
+        .required("El campo número de tarjeta no puede estar vacío"),
+      expiracion: Yup.date()
+        .required("El campo fecha de expiración no puede estar vacío"),
+      codigo: Yup.number()
+        .required("El campo fecha de expiración no puede estar vacío")
+    })}
+  >
+
+    {props => {
+      const {
+        values,
+        touched,
+        errors,
+        handleChange,
+        handleBlur,
+        handleSubmit
+      } = props;
+
+
+
+      return (
+
+        <form onSubmit={handleSubmit}>
+          <div className="row global">
+            <div className="col-md-12 mb-3">
+              <label htmlFor="nombre">Nombre</label>
+              <input
+                name="nombre"
+                type="text"
+                placeholder="Nahuel"
+                value={values.nombre}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className="form-control"
+                required
+              />
+              {errors.nombre && touched.nombre && (
+                <div className="alert alert-danger mt-1">{errors.nombre}</div>
+              )}
             </div>
-            <button className="btn btn-primary btn-lg btn-block mb-5 mt-5" type="submit">Continuar</button>
+            <div className="col-md-12 mb-3">
+              <label htmlFor="apellido">Apellido</label>
+              <input
+                name="apellido"
+                type="text"
+                placeholder="Lopez"
+                value={values.apellido}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className='form-control'
+                required
+              />
+              {errors.apellido && touched.apellido && (
+                <div className="alert alert-danger mt-1">{errors.apellido}</div>
+              )}
+            </div>
+            <div className="col-md-12 mb-3">
+              <label htmlFor="email">Email</label>
+              <input
+                name="email"
+                type="email"
+                placeholder="nahuel@davinci.edu.ar"
+                value={values.email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className='form-control'
+                required
+              />
+              {errors.email && touched.email && (
+                <div className="alert alert-danger mt-1">{errors.email}</div>
+              )}
+            </div>
+            <div className="col-md-12 mb-3">
+              <label htmlFor="dni">DNI</label>
+              <input
+                name="dni"
+                type="text"
+                placeholder="11.222.333"
+                value={values.dni}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className='form-control'
+                required
+              />
+              {errors.dni && touched.dni && (
+                <div className="alert alert-danger mt-1">{errors.dni}</div>
+              )}
+            </div>
+            <div className="col-md-12 mb-3">
+              <label htmlFor="provincia">Provincia</label>
+              <select className="custom-select d-block w-100" id="provincia" placeholder="Elegi una provincia">
+                <option>Buenos Aires</option>
+                <option>Chaco</option>
+                <option>Formosa</option>
+                <option>Mendoza</option>
+                <option>San Luis</option>
+                <option>Salta</option>
+                <option>Entre Ríos</option>
+              </select>
+              {errors.provincia && touched.provincia && (
+                <div className="alert alert-danger mt-1">{errors.provincia}</div>
+              )}
+            </div>
+            <div className="col-md-12 mb-3">
+              <label htmlFor="numTarj">Número de tarjeta</label>
+              <input
+                name="numTarj"
+                type="text"
+                placeholder="1111 2222 3333 4444"
+                value={values.numTarj}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className='form-control'
+                required
+              />
+              {errors.numTarj && touched.numTarj && (
+                <div className="alert alert-danger mt-1">{errors.numTarj}</div>
+              )}
+            </div>
+            <div className="col-md-12 mb-3">
+              <label htmlFor="expiracion">Fecha de expiración</label>
+              <input
+                name="expiracion"
+                type="month"
+                placeholder="Fecha"
+                value={values.expiracion}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className='form-control'
+                required
+              />
+              {errors.expiracion && touched.expiracion && (
+                <div className="alert alert-danger mt-1">{errors.expiracion}</div>
+              )}
+            </div>
+            <div className="col-md-12 mb-3">
+              <label htmlFor="codigo">Código de seguridad</label>
+              <input
+                name="codigo"
+                type="text"
+                placeholder="123"
+                value={values.codigo}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                className='form-control'
+                required
+              />
+              {errors.codigo && touched.codigo && (
+                <div className="alert alert-danger mt-1">{errors.codigo}</div>
+              )}
+            </div>
+          </div>
+          <button className="btn btn-primary btn-lg btn-block mb-5 mt-5" type="submit">Continuar</button>
 
-          </form>
-        );
-      }}
-    </Formik>
+        </form>
+      );
+    }}
+  </Formik>
 
-    
-    
 
-    };
-  
-  export default ValidateLoginCheckout;
+
+
+};
+
+export default ValidateLoginCheckout;
--- src/views/Pago.js
+++ src/views/Pago.js
@@ -3,21 +3,14 @@ import ValidateLoginCheckout from "./../components/ValidateLoginCheckout";
 
 function Pago() {
 
-  
-
- 
-    return (<div class="container">
-    <div class="py-5 text-center">
-        
-        <h2 className="check">Checkout</h2>
-    </div>
-    <div className="checkout">
-     <ValidateLoginCheckout />
-     </div>
-    
-         
-       
-</div>);
+    return (
+        <div class="fondopantalla p-5">
+            <h2 className="check">Checkout</h2>
+            <div className="checkout">
+                <ValidateLoginCheckout />
+            </div>
+        </div>
+    );
 }
 
 export default Pago;
