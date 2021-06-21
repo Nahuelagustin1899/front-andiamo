bd905c63ae7fb120a8bcb867df730b8adc1e4de6
Nahuel Lopez <lopeznahuel1999@gmail.com>
2021-06-21 20:49:38 -0300

    Panel empresas

--- src/views/NuevasEmpresas.js
+++ src/views/NuevasEmpresas.js
@@ -3,7 +3,7 @@ import CargarEmpresa from "./../components/CargarEmpresa";
 
 function NuevasEmpresas(props) {
     return (
-        <div className="fondopantalla p-5">
+        <div className="fondopantalla p-3">
             <h1 className="mb-4 viajes-empresa">Crear nueva empresa</h1>
             <div className="container divs">
                 <CargarEmpresa
