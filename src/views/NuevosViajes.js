2c728e0ee2d0961319de4a35060acc00e62af2df
Nahuel Lopez <lopeznahuel1999@gmail.com>
2021-06-21 21:00:35 -0300

    Panel viajes

--- src/views/NuevosViajes.js
+++ src/views/NuevosViajes.js
@@ -4,13 +4,13 @@ import CargarViaje from "./../components/CargarViaje";
 
 function NuevosViajes(props) {
     return (
-        <div className="fondopantalla p-5">
-            <h1 className="mb-4 viajes-empresa">Crear nuevo viaje</h1>
-            <div className="container divs">
-                <CargarViaje notExitosa={props.notExitosa}
-                />
-            </div>
-        </div>);
+    <div className="fondopantalla p-3">
+       <h1 className="mb-4 viajes-empresa">Crear nuevo viaje</h1>
+        <div className="container divs">
+        <CargarViaje notExitosa={props.notExitosa}
+        />
+        </div>
+    </div>);
 }
 
 export default NuevosViajes;
