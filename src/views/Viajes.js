2040e6c21a56633a7623b983ca85e17599c9a7e8
Nahuel Lopez <lopeznahuel1999@gmail.com>
2021-07-03 15:15:57 -0300

    filtros

--- src/views/Viajes.js
+++ src/views/Viajes.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Cargando from "../components/Cargando";
 import viajesService from "./../services/viajes";
 import { Link } from "react-router-dom";
 import Modal from 'react-bootstrap/Modal';
@@ -13,10 +12,13 @@ import EditIcon from '@material-ui/icons/Edit.js';
 import DeleteIcon from '@material-ui/icons/Delete.js';
 import AddIcon from '@material-ui/icons/Add.js';
 import { makeStyles } from "@material-ui/core/styles";
+import { FaMoneyBillAlt } from "react-icons/fa";
+import { SiGooglecalendar } from "react-icons/si";
+import TextField from '@material-ui/core/TextField';
 
 const useStyles = makeStyles((theme) => ({
     margin: {
-        margin: theme.spacing(1)
+        marginBottom: theme.spacing(3)
     },
     extendedIcon: {
         marginRight: theme.spacing(1)
@@ -25,9 +27,10 @@ const useStyles = makeStyles((theme) => ({
 
 function Viajes(props) {
     const history = useHistory();
+    const [viajesAux, setViajesAux] = useState([]);
+    const [precio, setPrecio] = useState("");
+    const [salida, setSalida] = useState("");
     const [viajes, setViajes] = useState([]);
-    const [cargando, setCargando] = useState(true);
-    const [search, setSearch] = useState("");
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -41,134 +44,165 @@ function Viajes(props) {
         (async () => {
             const data = await viajesService.indexEmpresa();
             setViajes(data);
-            setCargando(false);
+            setViajesAux(data);
         })().catch(err => console.log("Error al traer los viajes: ", err));
     }, []);
 
-    return (
-    <div className="fondopantalla p-5" >
-        <h1 className="mb-5 viajes-empresa">Panel de viajes</h1>
-        <div >
+    const filtro = () => {
+        
+        const newData = viajes.filter((item) => {
+            const itemPrecio = item.precio;
+            const itemSalida = item.fecha_salida;
+            return itemPrecio.indexOf(precio) > -1 && itemSalida.indexOf(salida) > -1;
+        });
 
-            <form className="busqueda mb-5">
-                <div className="form-group ">
-                    <label htmlFor="destino" className="labelcolor"><i className="mr-2 "><ImOffice style={{ fontSize: 28 }} /></i>Empresa</label>
-                    <input
-                        className="ml-2"
-                        type="text"
-                        placeholder="Buscar por empresa"
-                        onChange={(e) => setSearch(e.target.value)}
-                    />
-                </div>
-            </form>
+        console.log(newData);
+        setViajes(newData);
+    }
 
-            <Link to="/viajes/nueva">
-                <Fab variant="extended"
-                    size="medium"
-                    color="primary"
-                    aria-label="add"
-                    className={classes.margin}>
-                    <AddIcon className={classes.extendedIcon} />
-                Crear viaje
-            </Fab>
-            </Link>
-
-            {cargando ?
-                <Cargando /> :
-                (<div className="container-fluid">
-
-                    {viajes.filter((lista) =>
-                        lista.empresa.nombre.toLowerCase().includes(search.toLowerCase())).map(item => (<div key={item.id}>
-
-                            <Table variant="warning" striped bordered hover   >
-                                <thead>
-                                    <tr className="row">
-                                        <th className="col-4 text-center colorth">Salida</th>
-                                        <th className="col-4 text-center colorth">Llegada</th>
-                                        <th className="col-4 text-center colorth">Precio</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr className="row">
-                                        <td className="col-4 text-center colortd">{item.salida.nombre}</td>
-                                        <td className="col-4 text-center colortd">{item.destino.nombre}</td>
-                                        <td className="col-4 text-center colortd">$ {item.precio}</td>
-                                    </tr>
-                                </tbody>
-                            </Table>
-
-                            <Table variant="warning" striped bordered hover >
-                                <thead>
-                                    <tr className="row">
-                                        <th className="col-4 text-center colorth">Empresa</th>
-                                        <th className="col-4 text-center colorth">Fecha salida</th>
-                                        <th className="col-4 text-center colorth">Fecha llegada</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr className="row">
-                                        <td className="col-4 text-center colortd">{item.empresa.nombre}</td>
-                                        <td className="col-4 text-center colortd">{format(new Date
-                                            (item.fecha_salida), 'dd-MM-yyyy hh:mm:ss a')}</td>
-                                        <td className="col-4 text-center colortd ">{format(new Date
-                                            (item.fecha_llegada), 'dd-MM-yyyy hh:mm:ss a')}</td>
-
-                                    </tr>
-                                </tbody>
-                            </Table>
-                            <div>
-                                <Fab id="botones" color="secondary" onClick={handleShow} aria-label="delete">
-                                    <DeleteIcon />
-                                </Fab>
-
-                                <Fab id="botones1" onClick={() => editarViaje(item)} color="primary" aria-label="edit">
-                                    <EditIcon />
-                                </Fab>
-
-                            </div>
-                            <hr className="hr" />
+    const clear = () => {
+        setPrecio('');
+        setSalida('');
+        setViajes(viajesAux);
+    }
 
 
-                            <Modal show={show} onHide={handleClose}>
-                                <Modal.Header closeButton><b className="text-center">De
+    const lista = viajes.map(item => (
+        <div key={item.id}>
+            <Table variant="warning" striped bordered hover   >
+                <thead>
+                    <tr className="row">
+                        <th className="col-4 text-center colorth">Salida</th>
+                        <th className="col-4 text-center colorth">Llegada</th>
+                        <th className="col-4 text-center colorth">Precio</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="row">
+                        <td className="col-4 text-center colortd">{item.salida.nombre}</td>
+                        <td className="col-4 text-center colortd">{item.destino.nombre}</td>
+                        <td className="col-4 text-center colortd">$ {item.precio}</td>
+                    </tr>
+                </tbody>
+            </Table>
+
+            <Table variant="warning" striped bordered hover >
+                <thead>
+                    <tr className="row">
+                        <th className="col-4 text-center colorth">Empresa</th>
+                        <th className="col-4 text-center colorth">Fecha salida</th>
+                        <th className="col-4 text-center colorth">Fecha llegada</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr className="row">
+                        <td className="col-4 text-center colortd">{item.empresa.nombre}</td>
+                        <td className="col-4 text-center colortd">{format(new Date
+                            (item.fecha_salida), 'dd-MM-yyyy hh:mm:ss a')}</td>
+                        <td className="col-4 text-center colortd ">{format(new Date
+                            (item.fecha_llegada), 'dd-MM-yyyy hh:mm:ss a')}</td>
+
+                    </tr>
+                </tbody>
+            </Table>
+            <div>
+                <Fab id="botones" color="secondary" onClick={handleShow} aria-label="delete">
+                    <DeleteIcon />
+                </Fab>
+
+                <Fab id="botones1" onClick={() => editarViaje(item)} color="primary" aria-label="edit">
+                    <EditIcon />
+                </Fab>
+
+            </div>
+            <hr className="hr" />
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton><b className="text-center">De
                     {" " + item.salida.nombre} a {item.destino.nombre}</b>
-                                </Modal.Header>
-                                <Modal.Body>¿Estás seguro que deseas eliminar este viaje?</Modal.Body>
-                                <Modal.Footer>
-                                    <Button variant="secondary" onClick={handleClose}>
-                                        <button className="btn btn-secondary">
-                                            Cerrar
+                </Modal.Header>
+                <Modal.Body>¿Estás seguro que deseas eliminar este viaje?</Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        <button className="btn btn-secondary">
+                            Cerrar
                         </button>
-                                    </Button>
-                                    <Button onClick={handleClose}>
-                                        <button className="btn btn-primary"
-                                            onClick={() => {
-                                                viajesService.delete(item.id)
-                                                    .then(data => {
-                                                        setViajes(viajes.filter(viaje => viaje.id !== item.id));
-                                                        if (typeof props.notExitosaEliminar === 'function') {
-                                                            props.notExitosaEliminar(data);
+                    </Button>
+                    <Button onClick={handleClose}>
+                        <button className="btn btn-primary"
+                            onClick={() => {
+                                viajesService.delete(item.id)
+                                    .then(data => {
+                                        setViajes(viajes.filter(viaje => viaje.id !== item.id));
+                                        if (typeof props.notExitosaEliminar === 'function') {
+                                            props.notExitosaEliminar(data);
 
-                                                        }
-                                                        history.push('/viajes');
-                                                    })
+                                        }
+                                        history.push('/viajes');
+                                    })
 
-                                                    .catch(err => {
-                                                        if (typeof props.notDenegadaEliminar === 'function') {
-                                                            props.notDenegadaEliminar(item);
-                                                        }
-                                                    });
-                                            }}>Eliminar</button>
-                                    </Button>
-                                </Modal.Footer>
-                            </Modal>
-                        </div>
-                        ))}
+                                    .catch(err => {
+                                        if (typeof props.notDenegadaEliminar === 'function') {
+                                            props.notDenegadaEliminar(item);
+                                        }
+                                    });
+                            }}>Eliminar</button>
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+        </div>
 
-                </div>)}
+    ))
+
+    return (
+        <div className="fondopantalla" >
+            <h1 className="mb-5 viajes-empresa">Panel de viajes</h1>
+            <div>
+
+            <div className="filtros">
+                <div className="form-group ">
+                    <label className="d-block " htmlFor="date">Fecha salida <SiGooglecalendar className="ml-2" style={{ fontSize: 23 }}/></label>  
+                    <TextField
+                            className="form-control inputs-filtros"
+                            type="date"
+                            value={salida}
+                            onChange={(e) => setSalida(e.target.value)}
+                        /> 
+                    <button className="btn btn-success d-inline-block w-25 btn-fecha" onClick={filtro}>Buscar</button>
+                </div>
+
+                <div className="form-group ">
+                    <label className="d-block" htmlFor="empresa">Precio <FaMoneyBillAlt className="ml-2" style={{ fontSize: 25 }}/></label>        
+                    <input
+                        className="form-control inputs-filtros"
+                        type="text"
+                        value={precio}
+                        placeholder="Buscar por precio"
+                        onChange={(e) => setPrecio(e.target.value)}
+                    />
+                     <button className="btn btn-success d-inline-block w-25" onClick={filtro}>Buscar</button>
+                </div>
 
 
-        </div> </div>);
+                <button className="btn btn-primary limpiar-filtro" onClick={clear}>Limpiar</button>
+            </div>
+            <div className="p-4">
+                <Link to="/viajes/nueva">
+                    <Fab variant="extended"
+                        size="medium"
+                        color="primary"
+                        aria-label="add"
+                        className={classes.margin}>
+                        <AddIcon className={classes.extendedIcon} />
+                        Crear viaje
+                    </Fab>
+                </Link>
+               
+                    {lista}  
+                </div>       
+            </div>
+        </div>
+    )
 }
 
 export default Viajes;
