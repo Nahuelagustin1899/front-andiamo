a8e53c4ff62acacdb1cb1fec276371157754a670
Nahuel Lopez <lopeznahuel1999@gmail.com>
2021-07-13 21:40:55 -0300

    auth

--- src/views/Perfil.js
+++ src/views/Perfil.js
@@ -10,7 +10,7 @@ import EditIcon from '@material-ui/icons/Edit.js';
 function Perfil() {
 
     const [reservas, setReservas] = useState([]);
-    const urlbase = "https://andiamo-back.herokuapp.com/imgs/perfiles/logos/";
+    const urlbase = "https://andiamo-back.herokuapp.com/imgs/perfiles/logos/" ;
 
     useEffect(() => {
         (async () => {
@@ -20,45 +20,8 @@ function Perfil() {
     }, []);
 
     const lista = reservas && reservas.map(reserva => (<div key={reserva.id}>
-        <Table variant="warning" striped bordered hover   >
-            <thead>
-                <tr className="row">
-                    <th className="col-4 text-center colorth">Nombre</th>
-                    <th className="col-4 text-center colorth">Fecha salida</th>
-                    <th className="col-4 text-center colorth">Fecha llegada</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr className="row">
-                    <td className="col-4 text-center colortd">{reserva.user.name}</td>
-                    <td className="col-4 text-center colortd">{format(new Date(reserva.viaje.fecha_salida), 'dd-MM-yyyy hh:mm:ss a')}</td>
-                    <td className="col-4 text-center colortd">{format(new Date(reserva.viaje.fecha_llegada), 'dd-MM-yyyy hh:mm:ss a')}</td>
-                </tr>
-            </tbody>
-        </Table>
 
-        <Table variant="warning" striped bordered hover   >
-            <thead>
-                <tr className="row">
-                    <th className="col-4 text-center colorth">Precio</th>
-                    <th className="col-4 text-center colorth">Salida</th>
-                    <th className="col-4 text-center colorth">Destino</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr className="row">
-                    <td className="col-4 text-center colortd">{reserva.viaje.precio}</td>
-                    <td className="col-4 text-center colortd">{reserva.viaje.salida.nombre}</td>
-                    <td className="col-4 text-center colortd">{reserva.viaje.destino.nombre}</td>
-                </tr>
-            </tbody>
-        </Table>
 
-        <hr className="perfileshr" />
-
-    </div>));
-
-    const listaEmpresa = reservas && reservas.map(reserva => (<div key={reserva.id}>
         <Table variant="warning" striped bordered hover   >
             <thead>
                 <tr className="row">
@@ -103,12 +66,12 @@ function Perfil() {
     console.log(authData);
 
     return (<div className='fondopantalla p-5'>
-
+        
         <h1 className="viajes">Perfil</h1>
         <Link to="/editarusuario">
-            <Fab color="primary" aria-label="edit">
-                <EditIcon />
-            </Fab>
+        <Fab color="primary" aria-label="edit">
+             <EditIcon />
+        </Fab>
         </Link>
 
         <div className="caja-perfil">
@@ -117,7 +80,6 @@ function Perfil() {
         </div>
         <h3 className="mt-5 text-center mb-5 badge badge-warning"><b>Pasajes Reservados</b></h3>
         {lista}
-        {listaEmpresa}
     </div>);
 }
 
