3f311bd567e82a6cc0864660aa7a519001a18968
Nahuel Lopez <lopeznahuel1999@gmail.com>
2021-07-18 17:34:21 -0300

    auth

--- src/views/Perfil.js
+++ src/views/Perfil.js
@@ -70,48 +70,30 @@ function Perfil() {
     </div>));
  */
 
- const listaEmpresa = reservas && reservas.map(reserva => (<div key={reserva.id}>
+    const listaEmpresa = reservas && reservas.map(reserva => (<div key={reserva.id}>
 
 
         <Table variant="warning" striped bordered hover   >
             <thead>
                 <tr className="row">
                     <th className="col-4 text-center colorth">Nombre</th>
-                    <th className="col-4 text-center colorth">Fecha salida</th>
-                    <th className="col-4 text-center colorth">Fecha llegada</th>
+                    <th className="col-4 text-center colorth">Precio</th>
                 </tr>
             </thead>
             <tbody>
                 <tr className="row">
-                    <td className="col-4 text-center colortd">{reserva.user.name}</td>
-                    <td className="col-4 text-center colortd">{format(new Date(reserva.viaje.fecha_salida), 'dd-MM-yyyy hh:mm:ss a')}</td>
-                    <td className="col-4 text-center colortd">{format(new Date(reserva.viaje.fecha_llegada), 'dd-MM-yyyy hh:mm:ss a')}</td>
+                    <td className="col-4 text-center colortd">{reserva.nombre}</td>
+                    <td className="col-4 text-center colortd">{reserva.reservas[0].asiento_reservado}</td>
                 </tr>
             </tbody>
         </Table>
 
-        <Table variant="warning" striped bordered hover   >
-            <thead>
-                <tr className="row">
-                    <th className="col-4 text-center colorth">Precio</th>
-                    <th className="col-4 text-center colorth">Salida</th>
-                    <th className="col-4 text-center colorth">Destino</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr className="row">
-                    <td className="col-4 text-center colortd">{reserva.viaje.precio}</td>
-                    <td className="col-4 text-center colortd">{reserva.viaje.salida.nombre}</td>
-                    <td className="col-4 text-center colortd">{reserva.viaje.destino.nombre}</td>
-                </tr>
-            </tbody>
-        </Table>
 
 
 
         <hr className="perfileshr" />
 
-    </div>)); 
+    </div >));
 
 
     const authData = useContext(AuthContext);
@@ -131,7 +113,7 @@ function Perfil() {
             <p className="text-center badge badge-warning nombre-perfil"><b>Nombre :</b>  {authData.user.name}</p>
         </div>
         <h3 className="mt-5 text-center mb-5 badge badge-warning"><b>Pasajes Reservados</b></h3>
-      {/*   {lista} */}
+        {/*   {lista} */}
         {listaEmpresa}
     </div>);
 }
