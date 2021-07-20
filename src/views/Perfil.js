e3699c32eb24857de006ed1e3244c770c4b249bd
Nahuel Lopez <lopeznahuel1999@gmail.com>
2021-07-20 15:50:59 -0300

    auth

--- src/views/Perfil.js
+++ src/views/Perfil.js
@@ -89,7 +89,7 @@ function Perfil() {
             <tbody>
                 <tr className="row">
                    {reserva.reservas.map(espacio => <td className="col-4 text-center colortd">
-                       {espacio}</td>)}
+                       {espacio.asiento_reservado }</td>)}
                 </tr>
             </tbody>
         </Table>
