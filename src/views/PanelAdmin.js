21fd79181d4bf6e34495d5c935dcd8d73c18667c
Nahuel Lopez <lopeznahuel1999@gmail.com>
2021-07-21 20:16:45 -0300

    panel admin

--- src/views/PanelAdmin.js
+++ src/views/PanelAdmin.js
@@ -110,8 +110,8 @@ function PanelAdmin(props) {
             <hr className="hr" />
 
             <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton><b className="text-center">
-                    De {" " + item.salida.nombre}  <hr/> <br/> A {item.destino.nombre}</b>
+                <Modal.Header closeButton><b>
+                    De :{" " + item.salida.nombre}  <hr/> <br/> A: {item.destino.nombre}</b>
                 </Modal.Header>
                 <Modal.Body>¿Estás seguro que deseas eliminar este viaje?</Modal.Body>
                 <Modal.Footer>
