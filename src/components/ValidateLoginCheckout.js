c3fef58585a2c7ed147f837656bfa0c72f37038f
Nahuel Lopez <lopeznahuel1999@gmail.com>
2021-07-22 14:09:29 -0300

    check

--- src/components/ValidateLoginCheckout.js
+++ src/components/ValidateLoginCheckout.js
@@ -4,12 +4,21 @@ import * as Yup from "yup";
 import { useHistory } from "react-router";
 import { Link } from "react-router-dom";
 import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
 
 const ValidateLoginCheckout = (props) => {
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: '25ch',
+      },
+    },
+  }));
 
   const history = useHistory();
 
-  
+
 
   return <Formik
     initialValues={{
@@ -41,12 +50,12 @@ const ValidateLoginCheckout = (props) => {
         .required("El campo email no puede estar vacío"),
       dni: Yup.number()
         .required("El campo DNI no puede estar vacío")
-        .min(8,'No podes poner menos de 8 números')
-        .max(8,'No podes poner más de 8 números'),
+        .min(8, 'No podes poner menos de 8 números')
+        .max(8, 'No podes poner más de 8 números'),
       numTarj: Yup.number()
         .required("El campo número de tarjeta no puede estar vacío")
-        .min(16,'No podes poner menos de 16 números')
-        .max(16,'No podes poner más de 16 números'),
+        .min(16, 'No podes poner menos de 16 números')
+        .max(16, 'No podes poner más de 16 números'),
       expiracion: Yup.date()
         .required("El campo fecha de expiración no puede estar vacío"),
     })}
@@ -62,103 +71,104 @@ const ValidateLoginCheckout = (props) => {
         handleSubmit
       } = props;
 
+        const classes = useStyles();
+
+        return (
+
+          <form onSubmit={handleSubmit} className={classes.root} noValidate autoComplete="off">
+            <div className="row global">
+              <TextField id="standard-basic" label="Standard" />
+              <div className="col-md-12 mb-3">
+                <input
+                  name="nombre"
+                  type="text"
+                  placeholder="Nombre del titular de la tarjeta"
+                  value={values.nombre}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className="form-control"
+
+                />
+                {errors.nombre && touched.nombre && (
+                  <div className="alert alert-danger mt-1">{errors.nombre}</div>
+                )}
+              </div>
+
+              <div className="col-md-12 mb-3">
+                <input
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                  value={values.email}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className='form-control'
+
+                />
+                {errors.email && touched.email && (
+                  <div className="alert alert-danger mt-1">{errors.email}</div>
+                )}
+              </div>
+
+              <div className="col-md-12 mb-3">
+                <input
+                  name="dni"
+                  type="text"
+                  placeholder="Dni del titular de la tarjeta"
+                  value={values.dni}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className='form-control'
+
+                />
+                {errors.dni && touched.dni && (
+                  <div className="alert alert-danger mt-1">{errors.dni}</div>
+                )}
+              </div>
 
 
-      return (
+              <div className="col-md-12 mb-3">
+                <input
+                  name="numTarj"
+                  type="text"
+                  placeholder="Número de tarjeta"
+                  value={values.numTarj}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className='form-control'
 
-        <form onSubmit={handleSubmit}>
-          <div className="row global">
-          <TextField id="standard-basic" label="Standard" />
-            <div className="col-md-12 mb-3">
-              <input
-                name="nombre"
-                type="text"
-                placeholder="Nombre del titular de la tarjeta"
-                value={values.nombre}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                className="form-control"
-                
-              />
-              {errors.nombre && touched.nombre && (
-                <div className="alert alert-danger mt-1">{errors.nombre}</div>
-              )}
+                />
+                {errors.numTarj && touched.numTarj && (
+                  <div className="alert alert-danger mt-1">{errors.numTarj}</div>
+                )}
+              </div>
+
+
+              <div className="col-md-12 mb-3">
+                <label htmlFor="expiracion">Fecha de vencimiento</label>
+                <input
+                  name="expiracion"
+                  type="month"
+                  placeholder="Fecha"
+                  value={values.expiracion}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  className='form-control'
+
+                />
+                {errors.expiracion && touched.expiracion && (
+                  <div className="alert alert-danger mt-1">{errors.expiracion}</div>
+                )}
+              </div>
+
             </div>
-            
-            <div className="col-md-12 mb-3">
-              <input
-                name="email"
-                type="email"
-                placeholder="Email"
-                value={values.email}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                className='form-control'
-                
-              />
-              {errors.email && touched.email && (
-                <div className="alert alert-danger mt-1">{errors.email}</div>
-              )}
-            </div>
-
-            <div className="col-md-12 mb-3">
-              <input
-                name="dni"
-                type="text"
-                placeholder="Dni del titular de la tarjeta"
-                value={values.dni}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                className='form-control'
-                
-              />
-              {errors.dni && touched.dni && (
-                <div className="alert alert-danger mt-1">{errors.dni}</div>
-              )}
-            </div>
-            
-
-            <div className="col-md-12 mb-3">
-              <input
-                name="numTarj"
-                type="text"
-                placeholder="Número de tarjeta"
-                value={values.numTarj}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                className='form-control'
-                
-              />
-              {errors.numTarj && touched.numTarj && (
-                <div className="alert alert-danger mt-1">{errors.numTarj}</div>
-              )}
-            </div>
-   
-
-            <div className="col-md-12 mb-3">
-              <label htmlFor="expiracion">Fecha de vencimiento</label>
-              <input
-                name="expiracion"
-                type="month"
-                placeholder="Fecha"
-                value={values.expiracion}
-                onChange={handleChange}
-                onBlur={handleBlur}
-                className='form-control'
-                
-              />
-              {errors.expiracion && touched.expiracion && (
-                <div className="alert alert-danger mt-1">{errors.expiracion}</div>
-              )}
-            </div>
-          
-          </div>
-          <Link onClick={handleSubmit} className="btn btn-primary btn-lg btn-block mb-5 mt-5" type="submit">Finalizar reserva</Link>
+            <Link onClick={handleSubmit} className="btn btn-primary btn-lg btn-block mb-5 mt-5" type="submit">Finalizar reserva</Link>
 
 
-        </form>
-      );
-    }}
+          </form>
+        );
+      }
+    }
   </Formik>
 
 
