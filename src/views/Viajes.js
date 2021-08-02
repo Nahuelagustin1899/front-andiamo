d0893a492a944719d6f322cd1fcefc2fb72e04f3
Nahuel Lopez <lopeznahuel1999@gmail.com>
2021-08-02 16:49:46 -0300

    radius

--- src/views/Viajes.js
+++ src/views/Viajes.js
@@ -71,16 +71,16 @@ function Viajes(props) {
             <Table variant="warning" striped bordered hover   >
                 <thead>
                     <tr className="row">
-                        <th className="col-4 text-center colorth">Salida</th>
+                        <th className="col-4 text-center colorth radius">Salida</th>
                         <th className="col-4 text-center colorth">Llegada</th>
-                        <th className="col-4 text-center colorth">Precio</th>
+                        <th className="col-4 text-center colorth radius">Precio</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr className="row">
-                        <td className="col-4 text-center colortd">{item.salida.nombre}</td>
+                        <td className="col-4 text-center colortd radius">{item.salida.nombre}</td>
                         <td className="col-4 text-center colortd">{item.destino.nombre}</td>
-                        <td className="col-4 text-center colortd">$ {item.precio}</td>
+                        <td className="col-4 text-center colortd radius">$ {item.precio}</td>
                     </tr>
                 </tbody>
             </Table>
@@ -88,17 +88,17 @@ function Viajes(props) {
             <Table variant="warning" striped bordered hover >
                 <thead>
                     <tr className="row">
-                        <th className="col-4 text-center colorth">Empresa</th>
+                        <th className="col-4 text-center colorth radius">Empresa</th>
                         <th className="col-4 text-center colorth">Fecha salida</th>
-                        <th className="col-4 text-center colorth">Fecha llegada</th>
+                        <th className="col-4 text-center colorth radius">Fecha llegada</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr className="row">
-                        <td className="col-4 text-center colortd">{item.empresa.nombre}</td>
+                        <td className="col-4 text-center colortd radius">{item.empresa.nombre}</td>
                         <td className="col-4 text-center colortd">{format(new Date
                             (item.fecha_salida), 'dd-MM-yyyy hh:mm:ss a')}</td>
-                        <td className="col-4 text-center colortd ">{format(new Date
+                        <td className="col-4 text-center colortd radius">{format(new Date
                             (item.fecha_llegada), 'dd-MM-yyyy hh:mm:ss a')}</td>
 
                     </tr>
