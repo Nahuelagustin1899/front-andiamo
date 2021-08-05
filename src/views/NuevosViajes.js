7a9fc16f188c9512bdeba2f7e8d42b0020334ece
Nahuel Lopez <lopeznahuel1999@gmail.com>
2021-08-05 16:18:40 -0300

    icon

--- src/views/NuevosViajes.js
+++ src/views/NuevosViajes.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import CargarViaje from "./../components/CargarViaje";
-
+import KeyboardReturnSharpIcon from '@material-ui/icons/KeyboardReturnSharp';
 
 function NuevosViajes(props) {
     return (
     <div className="fondopantalla p-3">
        <h1 className="mb-4 viajes-empresa">Crear nuevo viaje</h1>
+       <a className="btn btn-primary mb-4" href="/viajes"><KeyboardReturnSharpIcon/></a>
         <div className="container divs">
         <CargarViaje notExitosa={props.notExitosa}
         />
