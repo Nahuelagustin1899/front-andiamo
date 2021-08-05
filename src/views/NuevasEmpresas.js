b202d9d2e9f5ac798c9be84fa762e62d58ea0572
Nahuel Lopez <lopeznahuel1999@gmail.com>
2021-08-05 16:43:20 -0300

    volver panel empresas

--- src/views/NuevasEmpresas.js
+++ src/views/NuevasEmpresas.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import CargarEmpresa from "./../components/CargarEmpresa";
+import KeyboardReturnSharpIcon from '@material-ui/icons/KeyboardReturnSharp';
 
 function NuevasEmpresas(props) {
     return (
         <div className="fondopantalla p-3">
+            <a className="btn btn-primary mb-4" href="/empresaspanel"><KeyboardReturnSharpIcon/></a>
             <h1 className="mb-4 viajes-empresa">Crear nueva empresa</h1>
             <div className="container divs">
                 <CargarEmpresa
